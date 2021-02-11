9f8422b3d4e78483b36ce2a90b7a3793b1cb79ad
Louise Barbarin <77415694+Hyziel@users.noreply.github.com>
2021-02-11 20:14:09 +0100

    Update IntermediateJavascriptExercise3.js

--- Intermediate Javascript/IntermediateJavascriptExercise3.js
+++ Intermediate Javascript/IntermediateJavascriptExercise3.js
@@ -3,7 +3,6 @@
 //Use arrays to check if your a person is on your guestlist
 
 function welcomeGuest(yourName){
- var yourName = prompt("What is your name ?");
  var guestList = ["Pierre", "Paul", "Jules", "Marie", "Lola", "Sophie"];
 
  if (guestList.includes(yourName)){
@@ -14,4 +13,5 @@ function welcomeGuest(yourName){
  }
 }
 
+var yourName = prompt("What is your name ?");
 var welcomeGuest = welcomeGuest(yourName);
